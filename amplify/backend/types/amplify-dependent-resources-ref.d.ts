a5a8f996f555cfd883b189b9d3596c49095e9693
Greg Shaffer <recreation.direct@gmail.com>
2021-12-15 17:25:10 -0800

    auth change try 1

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "cognitocf0c6096": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "AppClientSecret": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
